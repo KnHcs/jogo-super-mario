f6af705279a945c286cbd5bc892a6fbbb1faefea
KnHcs <163007643+KnHcs@users.noreply.github.com>
2024-08-28 10:49:23 -0300

    Update main.js

--- main.js
+++ main.js
@@ -4,13 +4,13 @@ const pipe = document.querySelector(".pipe");
 const jump = () => {
     mario.classList.add("jump");
 
-    setTimeout(() => {
-        mario.classList.remove("jump");
-    } 
-    ,500);
+ setTimeout(() => {
+    mario.classList.remove("jump");
+    }
+    ,500);   
 }
 
-const loop = setInterval (() => (();
+const loop = setInterval (() => {});
 
 
 
@@ -32,4 +32,6 @@ const loop = setInterval (() => (();
 
 
 
-document.addEventListener("keydown" , jump);
+
+
+document.addEventListener('keydown', jump);
