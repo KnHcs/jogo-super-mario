1e2cbc8547936178574acf92a50977645b70dc61
KnHcs <163007643+KnHcs@users.noreply.github.com>
2024-09-30 10:41:44 -0300

    Update main.js

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ const startButton = document.querySelector(".start");
 const gameOverScreen = document.querySelector(".game-over");
 const scoreElement = document.querySelector(".score");
 
-audioStart = new Audio("./sound/audio_theme.mp3");
+audioStart = new Audio("./sound/TROPA DO GORDÃO, MC Luizinho, MC Leon, DJ Batata, Feat DJ Lula do Jaca- Clipe Oficial #TropadoGordão.mp3");
 const gameOverSound = new Audio("./sound/audio_gameover.mp3");
 
 let gameStarted = false;
